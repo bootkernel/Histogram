9642dd7985f2521a2e2aa167b3f9c52724e26afb
Subha7 <33711869+bootkernel@users.noreply.github.com>
2018-09-21 19:04:28 -0700

    Add files via upload

---
+++ histogram.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+int ch;
+
+while((ch = getchar()) != EOF) {
+
+if(ch == ' ' || ch == '\n' || ch == '\t')
+	putchar('\n');
+else
+	putchar('*');
+}
+
+return 0;
+}
