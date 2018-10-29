afa8c6a4511627916970971aa8899303ef4e95b3
Subha7 <33711869+bootkernel@users.noreply.github.com>
2018-10-28 17:26:17 -0700

    removed spaces

--- histogram.c
+++ histogram.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 int main(){
-
 int ch;
-
+	
 while((ch = getchar()) != EOF) {
-
 if(ch == ' ' || ch == '\n' || ch == '\t')
 	putchar('\n');
 else
 	putchar('*');
 }
-
 return 0;
 }
